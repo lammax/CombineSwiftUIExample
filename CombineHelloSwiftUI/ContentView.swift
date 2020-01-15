f6eab923258d5862b051c01d325dc9888b8248af
Lammax <lammax@mail.ru>
2020-01-15 19:57:08 +0300

    Initial Commit

---
+++ CombineHelloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineHelloSwiftUI
+//
+//  Created by Mac on 15.01.2020.
+//  Copyright © 2020 Lammax. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
