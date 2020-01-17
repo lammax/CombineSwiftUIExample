cd1b3f793d5c34a1dc6d14199ddf6d6e5572c95a
Lammax <lammax@mail.ru>
2020-01-17 19:35:28 +0300

    Task model

---
+++ Lib/Models/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  CombineHelloSwiftUI
+//
+//  Created by Mac on 17.01.2020.
+//  Copyright © 2020 Lammax. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct Task: Identifiable {
+    
+    let id = UUID()
+    let name: String
+    
+}
